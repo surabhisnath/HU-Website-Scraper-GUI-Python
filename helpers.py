0ccfac0b1f9c8f49092e19a80adbccc016d5cf06
Surabhi S Nath <surabhi16271@iiitd.ac.in>
2021-09-18 16:23:25 +0200

    Add files via upload

--- helpers.py
+++ helpers.py
@@ -10,30 +10,30 @@ import validators
 
 def getrow(d, columns):
     """
-	Gets relevant information from student dictionary based on columns to include in spreadsheet
+    Gets relevant information from student dictionary based on columns to include in spreadsheet
 
-	Parameters:
-	    d (dict): contains one student's information as dictionary
-	    columns (list): list of columns to write on spreadsheet 
+    Parameters:
+        d (dict): contains one student's information as dictionary
+        columns (list): list of columns to write on spreadsheet
 
-	Returns:
-	    dict: relevant subset of dictionary based on columns to write on spreadsheet
+    Returns:
+        dict: relevant subset of dictionary based on columns to write on spreadsheet
 
-	"""
+    """
 
     return {i: d.get(i, "") for i in columns}
 
 
 def saveexcel(df, savepath, columns):
     """
-	Saves excel spreadsheet to specified location
+    Saves excel spreadsheet to specified location
 
-	Parameters:
-	    df (pd.Dataframe): Number of dice to roll
-	    savepath (str): Location to save spreadsheet
-	    columns (list): list of columns to write on spreadsheet
+    Parameters:
+        df (pd.Dataframe): Number of dice to roll
+        savepath (str): Location to save spreadsheet
+        columns (list): list of columns to write on spreadsheet
 
-	"""
+    """
 
     print("Saving spreadsheet...")
 
@@ -55,38 +55,45 @@ def saveexcel(df, savepath, columns):
 
 def getdetails(student):
     """
-	Parses all information for one student
+    Parses all information for one student
 
-	Parameters:
-	    student (bs4.element.Tag): student bs4 object 
+    Parameters:
+        student (bs4.element.Tag): student bs4 object
 
-	Returns:
-	    dict: contains student information as dictionary
+    Returns:
+        dict: contains student information as dictionary
 
-	Examples:
-		getdetails(bs4.element.Tag)
-		{'Name': 'Sanders, Lia', 'URLs': 'Researchgate: https://www.researchgate.net/profile/Lia_Sanders\n', 'Doctoral Project Title': 'Visual spatio-temporal integration in patients with Schizophrenia', 'Project Description': 'General theories of brain function assert that the brain works like an inference machine, whereby prior expectations are used to derive perceptual experiences from the sensory inputs. Impaired perceptual inference has been suggested as one possible causal factor underlying positive symptoms, and in particular delusions, in schizophrenia. According to this framework, delusional explanations are thought to reflect the attempt to cope with unusual percepts resulting from such impaired perceptual inference. During my PhD, we conducted a series of clinical studies testing this theoretical model. Methods: psychophysics, EEG, fMRI. \r\n', 'Funding': 'Mind and Brain scholarship\nMind and Brain postdoctoral scholarship', 'Supervisors': 'Prof. Dr. Philipp Sterzer\r\nProf. Dr. Andreas Heinz\r\nProf. Dr. Norbert Kathmann', 'M&B topics': 'Topic 1: Perception, attention, consciousness\nTopic 5: Brain disorders and mental dysfunction', 'Degrees obtained': 'MD, Federal University of Ceará – Fortaleza, Brazil', 'Institute': 'Klinik für Psychiatrie, CCM, Charité – Universitätsmedizin Berlin', 'Title': 'Dr.', 'Cohort': '2009', 'Status': 'Alumna', 'Researchgate': 'https://www.researchgate.net/profile/Lia_Sanders'}
+    Examples: getdetails(bs4.element.Tag) {'Name': 'Sanders, Lia', 'URLs': 'Researchgate:
+    https://www.researchgate.net/profile/Lia_Sanders\n', 'Doctoral Project Title': 'Visual spatio-temporal
+    integration in patients with Schizophrenia', 'Project Description': 'General theories of brain function assert
+    that the brain works like an inference machine, whereby prior expectations are used to derive perceptual
+    experiences from the sensory inputs. Impaired perceptual inference has been suggested as one possible causal
+    factor underlying positive symptoms, and in particular delusions, in schizophrenia. According to this framework,
+    delusional explanations are thought to reflect the attempt to cope with unusual percepts resulting from such
+    impaired perceptual inference. During my PhD, we conducted a series of clinical studies testing this theoretical
+    model. Methods: psychophysics, EEG, fMRI. \r\n', 'Funding': 'Mind and Brain scholarship\nMind and Brain
+    postdoctoral scholarship', 'Supervisors': 'Prof. Dr. Philipp Sterzer\r\nProf. Dr. Andreas Heinz\r\nProf. Dr.
+    Norbert Kathmann', 'M&B topics': 'Topic 1: Perception, attention, consciousness\nTopic 5: Brain disorders and
+    mental dysfunction', 'Degrees obtained': 'MD, Federal University of Ceará – Fortaleza, Brazil', 'Institute':
+    'Klinik für Psychiatrie, CCM, Charité – Universitätsmedizin Berlin', 'Title': 'Dr.', 'Cohort': '2009',
+    'Status': 'Alumna', 'Researchgate': 'https://www.researchgate.net/profile/Lia_Sanders'}
 
-	"""
+    """
 
     # Initialize dict
-    datadict = {}
-    datadict["Name"] = student.find('div', class_='students-list-item-full-name').find('a').text
-    datadict["URLs"] = ""
+    datadict = {"Name": student.find('div', class_='students-list-item-full-name').find('a').text, "URLs": ""}
 
     project_details = student.find('table')
     webnamestocolnames = {'Doctoral project': 'Doctoral Project Title',
                           'Description': 'Project Description'}  # Maps html tag name to column names
 
-    # Poppulates dictionary
+    # Populates dictionary
     for tr in project_details.findAll('tr'):
         td = tr.find('td')
         if tr.find('th').text == "E-mail" and td.text.find('@') >= 0 and td.text[td.text.find('@') + 1:].find('.') >= 0:
             towrite = td.text.replace("-please remove this text-", "")
         elif tr.find('th').text == "M&B topics":
             towrite = td.text[0] + td.text[1:].replace('Topic', '\nTopic')
-        elif tr.find('th').text == "Funding":
-            towrite = td.text.replace('; ', '\n')
         else:
             towrite = td.text
 
@@ -101,16 +108,16 @@ def getdetails(student):
 
 def makedataframe(student_data, columns):
     """
-	Creates and poppulates dataframe with scraped data
+    Creates and populates dataframe with scraped data
 
-	Parameters:
-		student_data (list): student information extracted from website
-		columns (list): List of columns to write on spreadsheet
+    Parameters:
+        student_data (list): student information extracted from website
+        columns (list): List of columns to write on spreadsheet
 
-	Returns:
-		pd.Dataframe: poppulated dataframe 
+    Returns:
+        pd.Dataframe: populated dataframe
 
-	"""
+    """
 
     print("Making dataframe...")
     dataframe = pd.DataFrame(columns=columns)
@@ -120,6 +127,6 @@ def makedataframe(student_data, columns):
         datadict = getdetails(student)
         row = getrow(datadict, columns)
         assert (len(row) == len(columns))
-        dataframe.loc[sid, :] = row;
+        dataframe.loc[sid, :] = row
 
     return dataframe
